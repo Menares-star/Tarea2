61cc8deb94b18763fdab576c82ca5772aff17441
Gabriel <gabriel.pezoa@sansano.usm.cl>
2020-11-26 13:58:53 -0300

    node2 y node3 creados

---
+++ src/node2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+	"fmt"
+	"google.golang.org/grpc"
+	"github.com/Menares-star/Tarea2/src/Mensajes"
+
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":7000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := Uploads.Server1{}
+
+	grpcServer := grpc.NewServer()
+
+	//REGISTRO DE SERVICIOS
+	Uploads.RegisterGuploadServiceServer(grpcServer, &s)
+	//ordenes.RegisterSeguimientoServiceServer(grpcServer, &s)
+	//FIN REGISTRO DE SERVICIOS
+	fmt.Println("NODO2 FUNCIONANDO")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
